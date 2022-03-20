53b530b00829800c88727d491b92d3edd8d8a4cb
Gabriela <gabriela@Gabrielas-MacBook-Pro.local>
2022-03-20 09:02:36 +0200

    Commission integration tests

--- tests/integration/commission.spec.js
+++ tests/integration/commission.spec.js
@@ -10,23 +10,160 @@ const should = chai.should();
 const server = require('../../src').app;
 const knex = require('../../src/db/knex');
 
-describe('routes: transactions', () => {
+describe('routes: commission', () => {
   beforeEach(() => knex.migrate.rollback()
     .then(() => knex.migrate.latest())
     .then(() => knex.seed.run()));
 
   afterEach(() => knex.migrate.rollback());
 
-  describe('GET /transactions', () => {
-    context('SUCCESS: Send GET request to list the transaction created', () => {
-      it('should return the transaction ', (done) => {
+  describe('POST /commission', () => {
+    context('SUCCESS: Send POST request to calculate commission', () => {
+      it('client with a discount: should return the commision amount and currency', (done) => {
         chai.request(server)
-          .get('/commission')
+          .post('/commission')
+          .set('Content-Type', 'application/json')
+          .send({
+            date: '2021-01-02',
+            amount: '2000.00',
+            currency: 'EUR',
+            client_id: 42,
+          })
           .end((err, res) => {
             should.not.exist(err);
             res.status.should.eql(200);
-            res.body[0].should.have.properties({
-              sum: '300.00'
+            res.type.should.eql('application/json');
+            res.body.should.have.properties({
+              amount: 0.05,
+              currency: 'EUR',
+            });
+            done();
+          });
+      });
+
+      it('default pricing: should return the commision amount and currency for default', (done) => {
+        chai.request(server)
+          .post('/commission')
+          .set('Content-Type', 'application/json')
+          .send({
+            date: '2021-01-03',
+            amount: '500.00',
+            currency: 'EUR',
+            client_id: 3,
+          })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.eql(200);
+            res.type.should.eql('application/json');
+            res.body.should.have.properties({
+              amount: 2.50,
+              currency: 'EUR',
+            });
+            done();
+          });
+      });
+
+      /* DATABASE SEED
+        { date: '2021-02-03', amount: 500, currency: 'EUR', client_id: 4}
+      */
+      it('default pricing: should return the commision amount and currency', (done) => {
+        chai.request(server)
+          .post('/commission')
+          .set('Content-Type', 'application/json')
+          .send({
+            date: '2021-01-04',
+            amount: '499.00',
+            currency: 'EUR',
+            client_id: 4,
+          })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.eql(200);
+            res.type.should.eql('application/json');
+            res.body.should.have.properties({
+              amount: 2.50,
+              currency: 'EUR',
+            });
+            done();
+          });
+      });
+
+      /* DATABASE SEED
+        { date: '2021-01-03', amount: 500, currency: 'EUR', client_id: 5 }
+        { date: '2021-01-04', amount: 499, currency: 'EUR', client_id: 5 }
+      */
+      it('default pricing with monthly turnover 999: should return the commision amount and currency', (done) => {
+        chai.request(server)
+          .post('/commission')
+          .set('Content-Type', 'application/json')
+          .send({
+            date: '2021-01-05',
+            amount: '100',
+            currency: 'EUR',
+            client_id: 5,
+          })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.eql(200);
+            res.type.should.eql('application/json');
+            res.body.should.have.properties({
+              amount: 0.50,
+              currency: 'EUR',
+            });
+            done();
+          });
+      });
+
+      /* DATABASE SEED
+        { date: '2021-01-03', amount: 500, currency: 'EUR', client_id: 1 },
+        { date: '2021-01-04', amount: 499, currency: 'EUR', client_id: 1 },
+        { date: '2021-01-05', amount: 100, currency: 'EUR', client_id: 1 }
+      */
+      it('high turnover discount: should return the commision amount and currency', (done) => {
+        chai.request(server)
+          .post('/commission')
+          .set('Content-Type', 'application/json')
+          .send({
+            date: '2021-01-06',
+            amount: '1.00',
+            currency: 'EUR',
+            client_id: 1,
+          })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.eql(200);
+            res.type.should.eql('application/json');
+            res.body.should.have.properties({
+              amount: 0.03,
+              currency: 'EUR',
+            });
+            done();
+          });
+      });
+
+      /* DATABASE SEED
+        { date: '2021-01-03', amount: 500, currency: 'EUR', client_id: 6 },
+        { date: '2021-01-04', amount: 499, currency: 'EUR', client_id: 6 },
+        { date: '2021-01-05', amount: 100, currency: 'EUR', client_id: 6 },
+        { date: '2021-01-06', amount: 1.00, currency: 'EUR', client_id: 6 }
+      */
+      it('default pricing previous month having high turnover: should return the commision amount and currency', (done) => {
+        chai.request(server)
+          .post('/commission')
+          .set('Content-Type', 'application/json')
+          .send({
+            date: '2021-02-01',
+            amount: '500',
+            currency: 'EUR',
+            client_id: 6,
+          })
+          .end((err, res) => {
+            should.not.exist(err);
+            res.status.should.eql(200);
+            res.type.should.eql('application/json');
+            res.body.should.have.properties({
+              amount: 2.5,
+              currency: 'EUR',
             });
             done();
           });
--- tests/integration/transactions.spec.js
+++ tests/integration/transactions.spec.js
@@ -24,46 +24,25 @@ describe('routes: transactions', () => {
           .post('/transactions')
           .set('Content-Type', 'application/json')
           .send({
-            date: '2021-01-01',
-            amount: '100.00',
+            date: '2021-01-17',
+            amount: '700.00',
             currency: 'EUR',
-            client_id: 42,
+            client_id: 50,
           })
           .end((err, res) => {
-            const parseDate = (value) => new Date(value);
             should.not.exist(err);
             res.status.should.eql(200);
             res.type.should.eql('application/json');
-
             const returnDate = new Date(res.body[0].date);
             res.body[0].date = `${returnDate.getFullYear()}-${(returnDate.getMonth() + 1).toString().padStart(2, '0')}-${returnDate.getDate().toString().padStart(2, '0')}`;
             res.body[0].should.have.properties({
-              date: '2021-01-01',
-              amount: '100.00',
+              date: '2021-01-17',
+              amount: '700.00',
               currency: 'EUR',
-              client_id: 42,
+              client_id: 50,
             });
+            res.body[0].should.have.property('id');
             res.body.should.have.length(1);
-            done();
-          });
-      });
-    });
-  });
-
-  describe('GET /transactions', () => {
-    context('SUCCESS: Send GET request to list the transaction created', () => {
-      it('should return the transaction ', (done) => {
-        chai.request(server)
-          .get('/transactions')
-          .end((err, res) => {
-            should.not.exist(err);
-            res.status.should.eql(200);
-            res.type.should.eql('application/json');
-            res.body[0].should.have.property('date');
-            res.body[0].should.have.property('amount');
-            res.body[0].should.have.property('currency');
-            res.body[0].should.have.property('client_id');
-
             done();
           });
       });
